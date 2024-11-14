9613da31cd67988c8878bf50dfefdfca1c22494a
KalesoneQ1451 <kacper.jatczak2006@gmail.com>
2024-11-14 13:31:54 +0100

    dziala

--- app/src/main/java/com/example/zadanie6/MainActivity.java
+++ app/src/main/java/com/example/zadanie6/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.zadanie6;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mail;
+    private EditText haslo;
+    private EditText reHaslo;
+    private Button przycisk;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        mail = findViewById(R.id.email);
+        haslo = findViewById(R.id.haslo);
+        reHaslo = findViewById(R.id.powtorz);
+        przycisk = findViewById(R.id.przycisk);
+        text = findViewById(R.id.tekst);
+
+        przycisk.setOnClickListener(view -> {
+            validation();
         });
+
+
+    }
+    public void validation(){
+        if (!mail.getText().toString().contains("@")){
+            text.setText("Niepoprawny adres e-mail");
+        } else if (!haslo.getText().toString().equals(reHaslo.getText().toString())) {
+            text.setText("Hasła się różnią");
+        } else {
+            text.setText("Witaj " + mail.getText());
+        }
+
     }
 }
